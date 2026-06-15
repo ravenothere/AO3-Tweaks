758b4c19524d980e675d6fa23cbceb6090ccc91f
Raven <moonshadow.ravenn@gmail.com>
2026-06-15 08:20:27 +0300

    Marked For Later section next to messages

--- add-ons/JavaScript/unread-messages-next-to-Favorites.js
+++ add-ons/JavaScript/unread-messages-next-to-Favorites.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AO3: Unread Messages next to Favorites
-// @version      1.0
+// @version      1.1
 // @description  Moves the Unread Messages section next to the Favorites list on the AO3 home page.
 // @author       ravenothere
 // @license      MIT
 // @match        *://archiveofourown.org/*
 // @grant        none
-// @namespace    https://greasyfork.org/users/1565271-ravenothere
+// @namespace    https://greasyfork.org/users/1565271
 // ==/UserScript==
 
 (function() {
@@ -14,8 +14,13 @@
 
 const favorites = document.querySelector('.favorite');
 const messages = document.querySelector('.latest.messages');
+const markedForLater = document.querySelector('.random.readings.module');
 
 if (favorites && messages) {
   favorites.after(messages);
+}
+
+if (messages && markedForLater) {
+  messages.after(markedForLater);
 }
 })();
